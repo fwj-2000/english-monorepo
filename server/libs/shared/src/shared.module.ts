b7b7afc2b1b73a5d8a3eec1e4ecfeb8b91ab8faa
xiaofu <2501292111@qq.com>
2026-04-26 22:28:08 +0800

    feat(prisma): 添加 Prisma 服务和模块到共享库
    将 PrismaService 和 PrismaModule 添加到共享库中，以便在 NestJS 应用中复用数据库连接。这为后续集成数据库操作提供了基础。

--- server/libs/shared/src/shared.module.ts
+++ server/libs/shared/src/shared.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SharedService } from './shared.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   providers: [SharedService],
   exports: [SharedService],
+  imports: [PrismaModule],
 })
 export class SharedModule {}
