f987050550fbbc6bb056f2877b3d4f1ac605796b
xiaofu <2501292111@qq.com>
2026-04-29 21:18:24 +0800

    feat(server): 添加JWT认证和配置模块

--- server/libs/shared/src/prisma/prisma.service.ts
+++ server/libs/shared/src/prisma/prisma.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@nestjs/common';
-import 'dotenv/config';
+// import 'dotenv/config';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '../generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-  constructor() {
-    const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+  constructor(configService: ConfigService) {
+    const dataBaseUrl = configService.get('DATABASE_URL');
+    const adapter = new PrismaPg({ connectionString: dataBaseUrl });//  new PrismaPg({ connectionString: process.env.DATABASE_URL });
+    console.log(dataBaseUrl, '数据库连接字符串');
     super({ adapter });
   }
-  getTest(): string {
-    return 'prisma test11';
-  }
+
 }
--- server/libs/shared/src/shared.module.ts
+++ server/libs/shared/src/shared.module.ts
@@ -2,10 +2,28 @@ import { Module, Global } from '@nestjs/common';
 import { SharedService } from './shared.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ResponseModule } from './response/response.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 @Global()
 @Module({
   providers: [SharedService],
-  exports: [SharedService, PrismaModule, ResponseModule],
-  imports: [PrismaModule, ResponseModule],
+  exports: [SharedService, PrismaModule, ResponseModule, JwtModule],
+  imports: [
+    PrismaModule,
+    ResponseModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        global: true,
+        secret: configService.get('SECRET_KEY'), //秘钥
+        signOptions: { expiresIn: 10 }, //10秒过期 这里方便测试  生产环境正常就是7天 7d
+      }),
+      inject: [ConfigService],
+    })],
 })
-export class SharedModule {}
+export class SharedModule { }
